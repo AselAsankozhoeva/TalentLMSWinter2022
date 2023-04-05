9da2db2325a1f2c0e03baf442a0a1eb5500fbeb5
Asel <asankozhoevaasel@gmail.com>
2023-04-05 12:19:03 +0600

    feature/GCPINT-1221

---
+++ src/main/java/com/digital/asserts/VisaPayment.java
@@ -0,0 +1,13 @@
+package com.digital.asserts;
+
+public class VisaPayment {
+
+    public void payViaVisa(String bank){
+        System.out.println("Some payment from: " + bank);
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
